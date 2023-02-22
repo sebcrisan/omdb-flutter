2d1a871545157d48474781b5c65dea8cc3c21911
Sebastiaan Crisan <sebastiaancrisan@gmail.com>
2023-02-22 12:52:42 +0100

    feat: :sparkles: add appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import "package:flutter/material.dart";
 
-void main() => runApp(const MaterialApp(
-      home: Text("Hello"),
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Movie Getter"),
+        ),
+      ),
     ));
