91ee8e9a221ce5861fc0680565b62abdf90b8b22
Sebastiaan Crisan <sebastiaancrisan@gmail.com>
2023-02-22 13:47:59 +0100

    refactor: :art: refactor into dry code

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,20 @@ final Logger logger = Logger();
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  static const String appTitle = "Movie Getter";
+  static const Color primaryColor = Color.fromARGB(255, 11, 17, 36);
+
   void buttonPressed() {
     logger.i('Button Pressed!');
+  }
+
+  TextStyle getTextStyle() {
+    return const TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.bold,
+      letterSpacing: 2.0,
+      color: Colors.grey,
+    );
   }
 
   @override
@@ -16,24 +28,19 @@ class MyApp extends StatelessWidget {
       key: const Key("Movie Getter"),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("Movie Getter"),
+          title: const Text(appTitle),
           centerTitle: true,
-          backgroundColor: Color.fromARGB(255, 11, 17, 36),
+          backgroundColor: primaryColor,
         ),
         body: const Center(
           child: Text(
             "Hello",
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.grey,
-            ),
+            style: TextStyle(),
           ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: buttonPressed,
-          backgroundColor: Color.fromARGB(255, 11, 17, 36),
+          backgroundColor: primaryColor,
           child: const Text("Click"),
         ),
       ),
