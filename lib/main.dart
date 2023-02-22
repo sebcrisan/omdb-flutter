1cce59e8ff68cbfdd7ca5474dc44e1e6fed8ecbb
Sebastiaan Crisan <sebastiaancrisan@gmail.com>
2023-02-22 13:03:21 +0100

    refactor: :recycle: allow hot reloading

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import "package:flutter/material.dart";
 
-void main() => runApp(MaterialApp(
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      key: const Key("Movie Getter"),
       home: Scaffold(
         appBar: AppBar(
           title: const Text("Movie Getter"),
         ),
       ),
-    ));
+    );
+  }
+}
